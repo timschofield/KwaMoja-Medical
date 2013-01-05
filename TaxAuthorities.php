c1ed04141d224e62565d551cc9ba59b20f58cfd8
Tim Schofield <tim@weberpafrica.com>
2013-01-05 14:25:03 +0000

    Lengthen the tax authority name field from 20 characters to 40

--- TaxAuthorities.php
+++ TaxAuthorities.php
@@ -256,7 +256,7 @@ if (!isset($_POST['Description'])) {
 echo '<table class="selection">
 		<tr>
 			<td>' . _('Tax Type Description') . ':</td>
-			<td><input type="text" name="Description" size="21" maxlength="20" value="' . $_POST['Description'] . '" /></td>
+			<td><input type="text" name="Description" size="21" maxlength="40" value="' . $_POST['Description'] . '" /></td>
 		</tr>
 		<tr>
 			<td>' . _('Input tax GL Account') . ':</td>

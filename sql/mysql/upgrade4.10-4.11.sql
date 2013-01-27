d0d8f73ccc6dd971abe5c8607c4e0b4a7749e37c
Phil Daintree <phil@logicworks.co.nz>
2013-01-27 12:04:01 +0000

    Add supplier discounts to purchasing data and change some code formatting

--- sql/mysql/upgrade4.10-4.11.sql
+++ sql/mysql/upgrade4.10-4.11.sql
@@ -1,1 +1,19 @@
+CREATE TABLE IF NOT EXISTS `supplierdiscounts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `supplierno` varchar(10) NOT NULL,
+  `stockid` varchar(20) NOT NULL,
+  `discountnarrative` varchar(20) NOT NULL,
+  `discountpercent` double NOT NULL,
+  `discountamount` double NOT NULL,
+  `effectivefrom` date NOT NULL,
+  `effectiveto` date NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `supplierno` (`supplierno`),
+  KEY `effectivefrom` (`effectivefrom`),
+  KEY `effectiveto` (`effectiveto`),
+  KEY `stockid` (`stockid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+
+
 UPDATE config SET confvalue='4.10.1' WHERE confname='VersionNumber';

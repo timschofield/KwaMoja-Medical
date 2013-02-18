d3775bfd1397deced5da8b4bbabf3f351ffc3503
Tim Schofield <tim@weberpafrica.com>
2013-02-18 13:48:31 +0000

    Further updates for new db update system

--- sql/updates/14.php
+++ sql/updates/14.php
@@ -1,8 +1,8 @@
 <?php
 
-ChangeColumnType('pagesecurity', 'scripts', 'INT', 'NOT NULL', 'DEFAULT 1', $db);
+ChangeColumnType('pagesecurity', 'scripts', 'INT(11)', 'NOT NULL', 'DEFAULT 1', $db);
 
-AddColumn('assigner', 'pctabs', 'VARCHAR', 'NOT NULL', "DEFAULT ''", 'tablimit', $db);
+AddColumn('assigner', 'pctabs', 'VARCHAR(20)', 'NOT NULL', "DEFAULT ''", 'tablimit', $db);
 
 ChangeConfigValue('VersionNumber', '4.04.1', $db);
 
---
+++ sql/updates/19.php
@@ -0,0 +1,50 @@
+<?php
+
+CreateTable('tenders',
+"CREATE TABLE `tenders` (
+  `tenderid` int(11) NOT NULL DEFAULT '0',
+  `location` varchar(5) NOT NULL DEFAULT '',
+  `address1` varchar(40) NOT NULL DEFAULT '',
+  `address2` varchar(40) NOT NULL DEFAULT '',
+  `address3` varchar(40) NOT NULL DEFAULT '',
+  `address4` varchar(40) NOT NULL DEFAULT '',
+  `address5` varchar(20) NOT NULL DEFAULT '',
+  `address6` varchar(15) NOT NULL DEFAULT '',
+  `telephone` varchar(25) NOT NULL DEFAULT '',
+  `closed` int(2) NOT NULL DEFAULT '0',
+  `requiredbydate` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`tenderid`)
+)",
+$db);
+
+CreateTable('tenderitems',
+"CREATE TABLE `tenderitems` (
+  `tenderid` int(11) NOT NULL DEFAULT '0',
+  `stockid` varchar(20) NOT NULL DEFAULT '',
+  `quantity` varchar(40) NOT NULL DEFAULT '',
+  `units` varchar(20) NOT NULL DEFAULT 'each',
+  PRIMARY KEY (`tenderid`,`stockid`)
+)",
+$db);
+
+CreateTable('tendersuppliers',
+"CREATE TABLE `tendersuppliers` (
+  `tenderid` int(11) NOT NULL DEFAULT '0',
+  `supplierid` varchar(10) NOT NULL DEFAULT '',
+  `email` varchar(40) NOT NULL DEFAULT '',
+  `responded` int(2) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`tenderid`,`supplierid`)
+)",
+$db);
+
+NewScript('SupplierTenderCreate.php', '4', $db);
+AddColumn('cancreatetender', 'www_users', 'TINYINT(1)', 'NOT NULL', 'DEFAULT 0', 'fullaccess', $db);
+InsertRecord('systypes', array('typeid', 'typename', 'typeno'), array('37', 'Tenders', '0'), array('typeid', 'typename', 'typeno'), array('37', 'Tenders', '0'), $db)
+
+NewConfigValue('StandardCostDecimalPlaces','2', $db);
+
+ChangeConfigValue('VersionNumber', '4.07', $db);
+
+UpdateDBNo(basename(__FILE__, '.php'), $db);
+
+?>

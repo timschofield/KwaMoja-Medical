e458448c2da2787131a9d9d6bd23a154f0ad6621
Exson Qu <hexinfrans@gmail.com>
2013-05-10 11:38:54 +0100

    10/5/2013 Exson Add mails group maintenance feature

---
+++ sql/updates/32.php
@@ -0,0 +1,23 @@
+<?php
+
+CreateTable('mailgroups',
+"CREATE TABLE `mailgroups` (
+	id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	groupname varchar(100) NOT NULL,
+	unique (groupname))",
+$db);
+
+CreateTable('sellthroughsupport',
+"CREATE TABLE `mailgroupdetails` (
+	groupname varchar(100) NOT NULL,
+	userid varchar(20) NOT NULL,
+	CONSTRAINT FOREIGN KEY (`groupname`) REFERENCES `mailgroups` (`groupname`),
+	CONSTRAINT FOREIGN KEY (`userid`) REFERENCES `www_users`(`userid`),
+	INDEX(`groupname`))",
+$db);
+
+NewScript('MailingGroupMaintenance.php',  '15', $db);
+
+UpdateDBNo(basename(__FILE__, '.php'), $db);
+
+?>

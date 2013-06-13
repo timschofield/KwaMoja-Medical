f3d4683b749a26ce2ef99ceb64caef483989babc
Tim Schofield <tim@weberpafrica.com>
2013-06-13 21:56:21 +0100

    Updates for moving menus to the database

--- sql/updates/42.php
+++ sql/updates/42.php
@@ -69,21 +69,67 @@ NewMenuItem('AR', 'Transactions', _('Create A Credit Note'), '/SelectCreditItems
 NewMenuItem('AR', 'Transactions', _('Enter Receipts'), '/CustomerReceipt.php?NewReceipt=Yes&amp;Type=Customer', 3, $db);
 NewMenuItem('AR', 'Transactions', _('Allocate Receipts or Credit Notes'), '/CustomerAllocations.php', 4, $db);
 
+NewMenuItem('AR', 'Reports', _('Where Allocated Inquiry'), '/CustWhereAlloc.php', 1, $db);
+NewMenuItem('AR', 'Reports', _('Print Invoices or Credit Notes'), '/PrintCustTrans.php', 2, $db);
+NewMenuItem('AR', 'Reports', _('Print Statements'), '/PrintCustStatements.php', 3, $db);
+NewMenuItem('AR', 'Reports', _('Sales Analysis Reports'), '/SalesAnalRepts.php', 4, $db);
+NewMenuItem('AR', 'Reports', _('Aged Customer Balances/Overdues Report'), '/AgedDebtors.php', 5, $db);
+NewMenuItem('AR', 'Reports', _('Re-Print A Deposit Listing'), '/PDFBankingSummary.php', 6, $db);
+NewMenuItem('AR', 'Reports', _('Debtor Balances At A Prior Month End'), '/DebtorsAtPeriodEnd.php', 7, $db);
+NewMenuItem('AR', 'Reports', _('Customer Listing By Area/Salesperson'), '/PDFCustomerList.php', 8, $db);
+NewMenuItem('AR', 'Reports', _('Sales Graphs'), '/SalesGraph.php', 9, $db);
+NewMenuItem('AR', 'Reports', _('List Daily Transactions'), '/PDFCustTransListing.php', 10, $db);
+NewMenuItem('AR', 'Reports', _('Customer Transaction Inquiries'), '/CustomerTransInquiry.php', 11, $db);
+
+NewMenuItem('AR', 'Maintenance', _('Add Customer'), '/Customers.php', 1, $db);
+NewMenuItem('AR', 'Maintenance', _('Select Customer'), '/SelectCustomer.php', 2, $db);
+
+NewMenuItem('AP', 'Transactions', _('Select Supplier'), '/SelectSupplier.php', 1, $db);
+NewMenuItem('AP', 'Transactions', _('Supplier Allocations'), '/SupplierAllocations.php', 2, $db);
+
+NewMenuItem('AP', 'Reports', _('Aged Supplier Report'), '/AgedSuppliers.php', 1, $db);
+NewMenuItem('AP', 'Reports', _('Payment Run Report'), '/SuppPaymentRun.php', 2, $db);
+NewMenuItem('AP', 'Reports', _('Remittance Advices'), '/PDFRemittanceAdvice.php', 3, $db);
+NewMenuItem('AP', 'Reports', _('Outstanding GRNs Report'), '/OutstandingGRNs.php', 4, $db);
+NewMenuItem('AP', 'Reports', _('Supplier Balances At A Prior Month End'), '/SupplierBalsAtPeriodEnd.php', 5, $db);
+NewMenuItem('AP', 'Reports', _('List Daily Transactions'), '/PDFSuppTransListing.php', 6, $db);
+NewMenuItem('AP', 'Reports', _('Supplier Transaction Inquiries'), '/SupplierTransInquiry.php', 7, $db);
+
+NewMenuItem('AP', 'Maintenance', _('Add Supplier'), '/Suppliers.php', 1, $db);
+NewMenuItem('AP', 'Maintenance', _('Select Supplier'), '/SelectSupplier.php', 2, $db);
+NewMenuItem('AP', 'Maintenance', _('Maintain Factor Companies'), '/Factors.php', 3, $db);
+
+NewMenuItem('PO', 'Transactions', _('Purchase Orders'), '/PO_SelectOSPurchOrder.php', 1, $db);
+NewMenuItem('PO', 'Transactions', _('Add Purchase Order'), '/PO_Header.php?NewOrder=Yes', 2, $db);
+NewMenuItem('PO', 'Transactions', _('Create a New Tender'), '/SupplierTenderCreate.php?New=Yes', 3, $db);
+NewMenuItem('PO', 'Transactions', _('Edit Existing Tenders'), '/SupplierTenderCreate.php?Edit=Yes', 4, $db);
+NewMenuItem('PO', 'Transactions', _('Process Tenders and Offers'), '/OffersReceived.php', 5, $db);
+NewMenuItem('PO', 'Transactions', _('Orders to Authorise'), '/PO_AuthoriseMyOrders.php', 6, $db);
+NewMenuItem('PO', 'Transactions', _('Shipment Entry'), '/SelectSupplier.php', 7, $db);
+NewMenuItem('PO', 'Transactions', _('Select A Shipment'), '/Shipt_Select.php', 8, $db);
+
+NewMenuItem('PO', 'Reports', _('Purchase Order Inquiry'), '/PO_SelectPurchOrder.php', 1, $db);
+NewMenuItem('PO', 'Reports', _('Purchase Order Detail Or Summary Inquiries'), '/POReport.php', 2, $db);
+NewMenuItem('PO', 'Reports', _('Supplier Price List'), '/SuppPriceList.php', 3, $db);
+
+NewMenuItem('PO', 'Maintenance', _('Maintain Supplier Price Lists'), '/SupplierPriceList.php', 1, $db);
+NewMenuItem('PO', 'Maintenance', _('Clear Orders with Quantity on Back Orders'), '/POClearBackOrders.php', 2, $db);
+
+NewMenuItem('stock', 'Transactions', _('Receive Purchase Orders'), '/PO_SelectOSPurchOrder.php', 1, $db);
+NewMenuItem('stock', 'Transactions', _('Bulk Inventory Transfer') . ' - ' . _('Dispatch'), '/StockLocTransfer.php', 2, $db);
+NewMenuItem('stock', 'Transactions', _('Bulk Inventory Transfer') . ' - ' . _('Receive'), '/StockLocTransferReceive.php', 3, $db);
+NewMenuItem('stock', 'Transactions', _('Inventory Location Transfers'), '/StockTransfers.php?New=Yes', 4, $db);
+NewMenuItem('stock', 'Transactions', _('Inventory Adjustments'), '/StockAdjustments.php?NewAdjustment=Yes', 5, $db);
+NewMenuItem('stock', 'Transactions', _('Reverse Goods Received'), '/ReverseGRN.php', 6, $db);
+NewMenuItem('stock', 'Transactions', _('Enter Stock Counts'), '/StockCounts.php', 7, $db);
+NewMenuItem('stock', 'Transactions', _('Create a New Internal Stock Request'), '/InternalStockRequest.php?New=Yes', 8, $db);
+NewMenuItem('stock', 'Transactions', _('Authorise Internal Stock Requests'), '/InternalStockRequestAuthorisation.php', 9, $db);
+NewMenuItem('stock', 'Transactions', _('Fulfil Internal Stock Requests'), '/InternalStockRequestFulfill.php', 10, $db);
 
 
 
 
-$MenuItems['AR']['Reports']['Caption'] = array (_('Where Allocated Inquiry'),
-												_('Print Invoices or Credit Notes'),
-												_('Print Statements'),
-												_('Sales Analysis Reports'),
-												_('Aged Customer Balances/Overdues Report'),
-												_('Re-Print A Deposit Listing'),
-												_('Debtor Balances At A Prior Month End'),
-												_('Customer Listing By Area/Salesperson'),
-												_('Sales Graphs'),
-												_('List Daily Transactions'),
-												_('Customer Transaction Inquiries')	);
+
 
 
 UpdateDBNo(basename(__FILE__, '.php'), $db);

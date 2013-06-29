01efb953c06a50794b64294c5b79b18e1a462d60
Pak Ricard <pakricard@gmail.com>
2013-06-29 09:17:48 +0100

    Country names comes from the array, not the currency table

--- SystemParameters.php
+++ SystemParameters.php
@@ -644,7 +644,7 @@ foreach ($CountriesArray as $CountryEntry => $CountryName) {
 	echo '<option ' . ($_SESSION['CountryOfOperation'] == $CountryEntry ? 'selected="selected" ' : '') . ' value="' . $CountryEntry . '">' . $CountryName . '</option>';
 }
 echo '</select></td>
-	<td>' . _('This parameter is only effective if Do Freight Calculation is set to Yes. Country names come from the currencies table.') . '</td></tr>';
+	<td>' . _('This parameter is only effective if Do Freight Calculation is set to Yes.') .'</td></tr>';
 
 // StandardCostDecimalPlaces
 echo '<tr style="outline: 1px solid"><td>' . _('Standard Cost Decimal Places') . ':</td>

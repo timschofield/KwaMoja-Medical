6d24cb019b4d97e49f4ace95e7a3d1013a553186
Phil Daintree <phil@logicworks.co.nz>
2013-07-06 22:53:29 +0100

    Shop config changes

--- sql/updates/51.php
+++ sql/updates/51.php
@@ -1,6 +1,8 @@
 <?php
 
 NewConfigValue('ShopShowLeftCategoryMenu', '1', $db);
+NewConfigValue('ShopStockLocations', '1', $db);
+DeleteConfigValue('ShopAdditionalStockLocations', $db);
 
 UpdateDBNo(basename(__FILE__, '.php'), $db);
 

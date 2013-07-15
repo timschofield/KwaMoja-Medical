2f0ee14a566d8c0664ba699c3210f41406f4e14e
Pak Ricard <pakricard@gmail.com>
2013-07-15 09:15:24 +0100

    Improvements on Title

--- FreightCosts.php
+++ FreightCosts.php
@@ -21,9 +21,9 @@ if (isset($_GET['SelectedFreightCost'])) {
 	$SelectedFreightCost = $_POST['SelectedFreightCost'];
 }
 
-echo '<div class="centre"><p class="page_title_text"><img src="'.$RootPath.'/css/'.$Theme.'/images/supplier.png" title="' . _('Freight Costs') . '" alt="" />' . ' ' . $Title . '</p></div>';
-
 if (!isset($LocationFrom) or !isset($ShipperID)) {
+	echo '<div class="centre"><p class="page_title_text"><img src="'.$RootPath.'/css/'.$Theme.'/images/supplier.png" title="' .
+		_('Freight Costs') . '" alt="" />' . ' ' . $Title . '</p></div>';
 
 	echo '<form onSubmit="return VerifyForm(this);" method="post" class="noPrint" action="' . htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, 'UTF-8') . '">';
 	echo '<div>';
@@ -78,6 +78,15 @@ if (!isset($LocationFrom) or !isset($ShipperID)) {
 	$LocationResults = DB_query($sql, $db);
 	$myrow = DB_fetch_row($LocationResults);
 	$LocationName = $myrow[0];
+	if (isset($ShipperID)){
+		$Title .= ' ' . _('For') . ' ' . $ShipperName;
+	}
+	if (isset($LocationFrom)){
+		$Title .= ' ' . _('From') . ' ' . $LocationName;
+	}
+
+	echo '<div class="centre"><p class="page_title_text"><img src="'.$RootPath.'/css/'.$Theme.'/images/supplier.png" title="' .
+		_('Freight Costs') . '" alt="" />' . ' ' . $Title . '</p></div>';
 
 }
 

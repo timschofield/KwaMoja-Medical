ef64f6362f6a6b4757c450826c2c615a30232863
Tim Schofield <tim@weberpafrica.com>
2013-07-22 11:20:38 +0100

    Script called by Ajax to update the favourites table

---
+++ UpdateFavourites.php
@@ -0,0 +1,28 @@
+<?php
+$PageSecurity=1;
+include('includes/session.inc');
+
+$sql = "SELECT caption
+			FROM favourites
+			WHERE userid='" . $_SESSION['UserID'] . "'
+				AND href='" . $_GET['Script'] . "'";
+$result = DB_query($sql, $db);
+
+if (DB_num_rows($result) == 0) {
+	$sql = "INSERT INTO favourites ( userid,
+									caption,
+									href
+								) VALUES (
+									'" . $_SESSION['UserID'] . "',
+									'" . $_GET['Title'] . "',
+									'" . $_GET['Script'] . "'
+								)";
+	$result = DB_query($sql, $db);
+} else {
+	$sql = "DELETE FROM favourites
+					WHERE userid='" . $_SESSION['UserID'] . "'
+						AND href='" . $_GET['Script'] . "'";
+	$result = DB_query($sql, $db);
+}
+
+?>

e009d4101dcfee11fd0385f3a8fc34bbfc5ceb44
Tim Schofield <tim@weberpafrica.com>
2013-09-24 19:34:03 +0100

    Demo data to load at install time

---
+++ sql/demodata/salesman.sql
@@ -0,0 +1,2 @@
+SET foreign_key_checks = 0;
+INSERT INTO `salesman` VALUES ('IN','Internet bookings','NB',1,'','',0,0,0,1),('Tim','Tim Schofield','KE',1,'','',0,0,0,1);
---
+++ sql/demodata/salestypes.sql
@@ -0,0 +1,2 @@
+SET foreign_key_checks = 0;
+INSERT INTO `salestypes` VALUES ('DE','Default'),('EA','East African Community'),('CR','MAD Casa'),('SA','MAD Sales');

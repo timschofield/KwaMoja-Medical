a0b30e01195cae9f7b1f8d5852653d681eb6d8db
Tim Schofield <tim@weberpafrica.com>
2013-09-24 19:47:05 +0100

    Go to menu at start up rather than the dashboard

--- includes/Login.php
+++ includes/Login.php
@@ -55,7 +55,7 @@ if (get_magic_quotes_gpc()) {
 		<tr>
 			<td width="70%">
 				<div id="login_box">
-					<form action="Dashboard.php" method="post" class="noPrint">
+					<form action="index.php" method="post" class="noPrint">
 					<input type="hidden" name="FormID" value="<?php
 echo $_SESSION['FormID'];
 ?>" />

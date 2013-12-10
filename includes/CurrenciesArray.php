4d693ff6ca1f6e01fc97e71c5075f952caed2629
Tim Schofield <tim@weberpafrica.com>
2013-12-10 21:52:00 +0000

    Improve code layout

--- includes/CurrenciesArray.php
+++ includes/CurrenciesArray.php
@@ -1,370 +1,192 @@
 <?php
-
 /*
 Currency codes based on the three-letter alphabetic code from
 ISO 4217:2008 Codes for the representation of currencies and funds.
 
 This program is under the GNU General Public License version 3.
 */
+
 $CurrenciesArray = array();
 
-
 $CurrenciesArray['AED']['Currency'] = _('United Arab Emirates dirham');
-
 $CurrenciesArray['AFN']['Currency'] = _('Afghan afghani');
-
 $CurrenciesArray['ALL']['Currency'] = _('Albanian lek');
-
 $CurrenciesArray['AMD']['Currency'] = _('Armenian dram');
-
 $CurrenciesArray['ANG']['Currency'] = _('Netherlands Antillean guilder');
-
 $CurrenciesArray['AOA']['Currency'] = _('Angolan kwanza');
-
 $CurrenciesArray['ARS']['Currency'] = _('Argentine peso');
-
 $CurrenciesArray['AUD']['Currency'] = _('Australian dollar');
-
 $CurrenciesArray['AWG']['Currency'] = _('Aruban florin');
-
 $CurrenciesArray['AZN']['Currency'] = _('Azerbaijani manat');
-
 $CurrenciesArray['BAM']['Currency'] = _('Bosnia and Herzegovina convertible mark');
-
 $CurrenciesArray['BBD']['Currency'] = _('Barbados dollar');
-
 $CurrenciesArray['BDT']['Currency'] = _('Bangladeshi taka');
-
 $CurrenciesArray['BGN']['Currency'] = _('Bulgarian lev');
-
 $CurrenciesArray['BHD']['Currency'] = _('Bahraini dinar');
-
 $CurrenciesArray['BIF']['Currency'] = _('Burundian franc');
-
 $CurrenciesArray['BMD']['Currency'] = _('Bermudian dollar');
-
 $CurrenciesArray['BND']['Currency'] = _('Brunei dollar');
-
 $CurrenciesArray['BOB']['Currency'] = _('Bolivian Boliviano');
-
 $CurrenciesArray['BOV']['Currency'] = _('Bolivian Mvdol (funds code)');
-
 $CurrenciesArray['BRL']['Currency'] = _('Brazilian real');
-
 $CurrenciesArray['BSD']['Currency'] = _('Bahamian dollar');
-
 $CurrenciesArray['BTN']['Currency'] = _('Bhutanese ngultrum');
-
 $CurrenciesArray['BWP']['Currency'] = _('Botswana pula');
-
 $CurrenciesArray['BYR']['Currency'] = _('Belarusian ruble');
-
 $CurrenciesArray['BZD']['Currency'] = _('Belize dollar');
-
 $CurrenciesArray['CAD']['Currency'] = _('Canadian dollar');
-
 $CurrenciesArray['CDF']['Currency'] = _('Congolese franc');
-
 $CurrenciesArray['CHE']['Currency'] = _('Swiss WIR Euro (complementary currency)');
-
 $CurrenciesArray['CHF']['Currency'] = _('Swiss franc');
-
 $CurrenciesArray['CHW']['Currency'] = _('Swiss WIR Franc (complementary currency)');
-
 $CurrenciesArray['CLF']['Currency'] = _('Chilean Unidad de Fomento (funds code)');
-
 $CurrenciesArray['CLP']['Currency'] = _('Chilean peso');
-
 $CurrenciesArray['CNY']['Currency'] = _('Chinese yuan');
-
 $CurrenciesArray['COP']['Currency'] = _('Colombian peso');
-
 $CurrenciesArray['COU']['Currency'] = _('Colombian Unidad de Valor Real');
-
 $CurrenciesArray['CRC']['Currency'] = _('Costa Rican colon');
-
 $CurrenciesArray['CUC']['Currency'] = _('Cuban peso convertible');
-
 $CurrenciesArray['CUP']['Currency'] = _('Cuban peso');
-
 $CurrenciesArray['CVE']['Currency'] = _('Cape Verde escudo');
-
 $CurrenciesArray['CZK']['Currency'] = _('Czech koruna');
-
 $CurrenciesArray['DJF']['Currency'] = _('Djiboutian franc');
-
 $CurrenciesArray['DKK']['Currency'] = _('Danish krone');
-
 $CurrenciesArray['DOP']['Currency'] = _('Dominican peso');
-
 $CurrenciesArray['DZD']['Currency'] = _('Algerian dinar');
-
 $CurrenciesArray['EGP']['Currency'] = _('Egyptian pound');
-
 $CurrenciesArray['ERN']['Currency'] = _('Eritrean nakfa');
-
 $CurrenciesArray['ETB']['Currency'] = _('Ethiopian birr');
-
 $CurrenciesArray['EUR']['Currency'] = _('European Union euro');
-
 $CurrenciesArray['FJD']['Currency'] = _('Fiji dollar');
-
 $CurrenciesArray['FKP']['Currency'] = _('Falkland Islands pound');
-
 $CurrenciesArray['GBP']['Currency'] = _('United Kingdom pound sterling');
-
 $CurrenciesArray['GEL']['Currency'] = _('Georgian lari');
-
 $CurrenciesArray['GHS']['Currency'] = _('Ghanaian cedi');
-
 $CurrenciesArray['GIP']['Currency'] = _('Gibraltar pound');
-
 $CurrenciesArray['GMD']['Currency'] = _('Gambian dalasi');
-
 $CurrenciesArray['GNF']['Currency'] = _('Guinean franc');
-
 $CurrenciesArray['GTQ']['Currency'] = _('Guatemalan quetzal');
-
 $CurrenciesArray['GYD']['Currency'] = _('Guyanese dollar');
-
 $CurrenciesArray['HKD']['Currency'] = _('Hong Kong dollar');
-
 $CurrenciesArray['HNL']['Currency'] = _('Honduran lempira');
-
 $CurrenciesArray['HRK']['Currency'] = _('Croatian kuna');
-
 $CurrenciesArray['HTG']['Currency'] = _('Haitian gourde');
-
 $CurrenciesArray['HUF']['Currency'] = _('Hungarian forint');
-
 $CurrenciesArray['IDR']['Currency'] = _('Indonesian rupiah');
-
 $CurrenciesArray['ILS']['Currency'] = _('Israeli new shekel');
-
 $CurrenciesArray['INR']['Currency'] = _('Indian rupee');
-
 $CurrenciesArray['IQD']['Currency'] = _('Iraqi dinar');
-
 $CurrenciesArray['IRR']['Currency'] = _('Iranian rial');
-
 $CurrenciesArray['ISK']['Currency'] = _('Icelandic króna');
-
 $CurrenciesArray['JMD']['Currency'] = _('Jamaican dollar');
-
 $CurrenciesArray['JOD']['Currency'] = _('Jordanian dinar');
-
 $CurrenciesArray['JPY']['Currency'] = _('Japanese yen');
-
 $CurrenciesArray['KES']['Currency'] = _('Kenyan shilling');
-
 $CurrenciesArray['KGS']['Currency'] = _('Kyrgyzstani som');
-
 $CurrenciesArray['KHR']['Currency'] = _('Cambodian riel');
-
 $CurrenciesArray['KMF']['Currency'] = _('Comoro franc');
-
 $CurrenciesArray['KPW']['Currency'] = _('North Korean won');
-
 $CurrenciesArray['KRW']['Currency'] = _('South Korean won');
-
 $CurrenciesArray['KWD']['Currency'] = _('Kuwaiti dinar');
-
 $CurrenciesArray['KYD']['Currency'] = _('Cayman Islands dollar');
-
 $CurrenciesArray['KZT']['Currency'] = _('Kazakhstani tenge');
-
 $CurrenciesArray['LAK']['Currency'] = _('Lao kip');
-
 $CurrenciesArray['LBP']['Currency'] = _('Lebanese pound');
-
 $CurrenciesArray['LKR']['Currency'] = _('Sri Lankan rupee');
-
 $CurrenciesArray['LRD']['Currency'] = _('Liberian dollar');
-
 $CurrenciesArray['LSL']['Currency'] = _('Lesotho loti');
-
 $CurrenciesArray['LTL']['Currency'] = _('Lithuanian litas');
-
 $CurrenciesArray['LVL']['Currency'] = _('Latvian lats');
-
 $CurrenciesArray['LYD']['Currency'] = _('Libyan dinar');
-
 $CurrenciesArray['MAD']['Currency'] = _('Moroccan dirham');
-
 $CurrenciesArray['MDL']['Currency'] = _('Moldovan leu');
-
 $CurrenciesArray['MGA']['Currency'] = _('Malagasy ariary');
-
 $CurrenciesArray['MKD']['Currency'] = _('Macedonian denar');
-
 $CurrenciesArray['MMK']['Currency'] = _('Myanmar Kyat');
-
 $CurrenciesArray['MNT']['Currency'] = _('Mongolian tugrik');
-
 $CurrenciesArray['MOP']['Currency'] = _('Macanese pataca');
-
 $CurrenciesArray['MRO']['Currency'] = _('Mauritanian ouguiya');
-
 $CurrenciesArray['MUR']['Currency'] = _('Mauritian rupee');
-
 $CurrenciesArray['MVR']['Currency'] = _('Maldivian rufiyaa');
-
 $CurrenciesArray['MWK']['Currency'] = _('Malawian kwacha');
-
 $CurrenciesArray['MXN']['Currency'] = _('Mexican peso');
-
 $CurrenciesArray['MXV']['Currency'] = _('Mexican Unidad de Inversion (funds code)');
-
 $CurrenciesArray['MYR']['Currency'] = _('Malaysian ringgit');
-
 $CurrenciesArray['MZN']['Currency'] = _('Mozambican metical');
-
 $CurrenciesArray['NAD']['Currency'] = _('Namibian dollar');
-
 $CurrenciesArray['NGN']['Currency'] = _('Nigerian naira');
-
 $CurrenciesArray['NIO']['Currency'] = _('Nicaraguan córdoba');
-
 $CurrenciesArray['NOK']['Currency'] = _('Norwegian krone');
-
 $CurrenciesArray['NPR']['Currency'] = _('Nepalese rupee');
-
 $CurrenciesArray['NZD']['Currency'] = _('New Zealand dollar');
-
 $CurrenciesArray['OMR']['Currency'] = _('Omani rial');
-
 $CurrenciesArray['PAB']['Currency'] = _('Panamanian balboa');
-
 $CurrenciesArray['PEN']['Currency'] = _('Peruvian nuevo sol');
-
 $CurrenciesArray['PGK']['Currency'] = _('Papua New Guinean kina');
-
 $CurrenciesArray['PHP']['Currency'] = _('Philippine peso');
-
 $CurrenciesArray['PKR']['Currency'] = _('Pakistani rupee');
-
 $CurrenciesArray['PLN']['Currency'] = _('Polish złoty');
-
 $CurrenciesArray['PYG']['Currency'] = _('Paraguayan guaraní');
-
 $CurrenciesArray['QAR']['Currency'] = _('Qatari riyal');
-
 $CurrenciesArray['RON']['Currency'] = _('Romanian new leu');
-
 $CurrenciesArray['RSD']['Currency'] = _('Serbian dinar');
-
 $CurrenciesArray['RUB']['Currency'] = _('Russian rouble');
-
 $CurrenciesArray['RWF']['Currency'] = _('Rwandan franc');
-
 $CurrenciesArray['SAR']['Currency'] = _('Saudi riyal');
-
 $CurrenciesArray['SBD']['Currency'] = _('Solomon Islands dollar');
-
 $CurrenciesArray['SCR']['Currency'] = _('Seychelles rupee');
-
 $CurrenciesArray['SDG']['Currency'] = _('Sudanese pound');
-
 $CurrenciesArray['SEK']['Currency'] = _('Swedish krona');
-
 $CurrenciesArray['SGD']['Currency'] = _('Singapore dollar');
-
 $CurrenciesArray['SHP']['Currency'] = _('Saint Helena pound');
-
 $CurrenciesArray['SLL']['Currency'] = _('Sierra Leonean leone');
-
 $CurrenciesArray['SOS']['Currency'] = _('Somali shilling');
-
 $CurrenciesArray['SRD']['Currency'] = _('Surinamese dollar');
-
 $CurrenciesArray['SSP']['Currency'] = _('South Sudanese pound');
-
 $CurrenciesArray['STD']['Currency'] = _('São Tomé and Príncipe dobra');
-
 $CurrenciesArray['SYP']['Currency'] = _('Syrian pound');
-
 $CurrenciesArray['SZL']['Currency'] = _('Swazi lilangeni');
-
 $CurrenciesArray['THB']['Currency'] = _('Thai baht');
-
 $CurrenciesArray['TJS']['Currency'] = _('Tajikistani somoni');
-
 $CurrenciesArray['TMT']['Currency'] = _('Turkmenistani manat');
-
 $CurrenciesArray['TND']['Currency'] = _('Tunisian dinar');
-
 $CurrenciesArray['TOP']['Currency'] = _('Tongan paʻanga');
-
 $CurrenciesArray['TRY']['Currency'] = _('Turkish lira');
-
 $CurrenciesArray['TTD']['Currency'] = _('Trinidad and Tobago dollar');
-
 $CurrenciesArray['TWD']['Currency'] = _('Taiwan new dollar');
-
 $CurrenciesArray['TZS']['Currency'] = _('Tanzanian shilling');
-
 $CurrenciesArray['UAH']['Currency'] = _('Ukrainian hryvnia');
-
 $CurrenciesArray['UGX']['Currency'] = _('Ugandan shilling');
-
 $CurrenciesArray['USD']['Currency'] = _('United States dollar');
-
 $CurrenciesArray['USN']['Currency'] = _('United States dollar next day (funds code)');
-
 $CurrenciesArray['USS']['Currency'] = _('United States dollar same day (funds code)');
-
 $CurrenciesArray['UYI']['Currency'] = _('Uruguayan unidad indexada (funds code)');
-
 $CurrenciesArray['UYU']['Currency'] = _('Uruguayan peso');
-
 $CurrenciesArray['UZS']['Currency'] = _('Uzbekistan som');
-
 $CurrenciesArray['VEF']['Currency'] = _('Venezuelan bolívar fuerte');
-
 $CurrenciesArray['VND']['Currency'] = _('Vietnamese dong');
-
 $CurrenciesArray['VUV']['Currency'] = _('Vanuatu vatu');
-
 $CurrenciesArray['WST']['Currency'] = _('Samoan tala');
-
 $CurrenciesArray['XAF']['Currency'] = _('CFA franc BEAC');
-
 $CurrenciesArray['XAG']['Currency'] = _('Silver (one troy ounce)');
-
 $CurrenciesArray['XAU']['Currency'] = _('Gold (one troy ounce)');
-
 $CurrenciesArray['XBA']['Currency'] = _('European Composite Unit');
-
 $CurrenciesArray['XBB']['Currency'] = _('European Monetary Unit');
-
 $CurrenciesArray['XBC']['Currency'] = _('European Unit of Account 9');
-
 $CurrenciesArray['XBD']['Currency'] = _('European Unit of Account 17');
-
 $CurrenciesArray['XCD']['Currency'] = _('East Caribbean dollar');
-
 $CurrenciesArray['XDR']['Currency'] = _('Special drawing rights');
-
 $CurrenciesArray['XFU']['Currency'] = _('UIC franc (special settlement currency)');
-
 $CurrenciesArray['XOF']['Currency'] = _('CFA franc BCEAO');
-
 $CurrenciesArray['XPD']['Currency'] = _('Palladium (one troy ounce)');
-
 $CurrenciesArray['XPF']['Currency'] = _('CFP franc');
-
 $CurrenciesArray['XPT']['Currency'] = _('Platinum (one troy ounce)');
-
 $CurrenciesArray['XTS']['Currency'] = _('Code reserved for testing purposes');
-
 $CurrenciesArray['XXX']['Currency'] = _('No currency');
-
 $CurrenciesArray['YER']['Currency'] = _('Yemeni rial');
-
 $CurrenciesArray['ZAR']['Currency'] = _('South African rand');
-
 $CurrenciesArray['ZMW']['Currency'] = _('Zambian kwacha');
 
-
 asort($CurrenciesArray);
+
 ?>

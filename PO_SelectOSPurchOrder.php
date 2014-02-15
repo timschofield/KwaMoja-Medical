993b35828639bc25ffe788ad900e569eb5b1bc97
vvs2012 <vvs2012@users.sourceforge.net>
2014-02-15 13:19:27 +0000

    Show all POs when enter PO_SelectOSPurchOrder.php page. Then allow filter by date range

--- PO_SelectOSPurchOrder.php
+++ PO_SelectOSPurchOrder.php
@@ -109,6 +109,7 @@ if (!isset($OrderNumber) or $OrderNumber == '') {
 		$DateRow = DB_fetch_array($DateResult);
 		$DateFrom = $DateRow['fromdate'];
 		$DateTo = $DateRow['todate'];
+		$_POST['SearchOrders'] = 'New';
 	} else {
 		$DateFrom = FormatDateForSQL($_POST['DateFrom']);
 		$DateTo = FormatDateForSQL($_POST['DateTo']);
@@ -257,7 +258,7 @@ if (isset($StockItemsResult)) {
 
 	echo '</table>';
 
-} else if (isset($_POST['SearchOrders'])) {
+} elseif (isset($_POST['SearchOrders'])) {
 	//figure out the SQL required from the inputs available
 
 	if (!isset($_POST['Status']) or $_POST['Status'] == 'Pending_Authorised') {
@@ -321,8 +322,8 @@ if (isset($StockItemsResult)) {
 					" . $OrderNumberSearchString . "
 					" . $StatusCriteria . "
 					" . $LocationSearchString . "
-					AND orddate>='" . FormatDateForSQL($_POST['DateFrom']) . "'
-					AND orddate<='" . FormatDateForSQL($_POST['DateTo']) . "'
+					AND orddate>='" . $DateFrom . "'
+					AND orddate<='" . $DateTo . "'
 				GROUP BY purchorders.orderno ASC,
 						suppliers.suppname,
 						purchorders.orddate,

720f6bfbab1c6e0f08a8a00b14850439ed0af038
Tim Schofield <tim@weberpafrica.com>
2014-03-22 10:15:58 +0000

    Countries array wasn't being included so couldn't show list of countries

--- Locations.php
+++ Locations.php
@@ -435,6 +435,7 @@ echo '<br />';
 
 if (!isset($_GET['delete'])) {
 
+	include('includes/CountriesArray.php');
 	echo '<form onSubmit="return VerifyForm(this);" method="post" class="noPrint" action="' . htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, 'UTF-8') . '">';
 	echo '<div>';
 	echo '<input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />';

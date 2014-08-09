157440aaec47b8f4a01fe5269a4b7b253addf1df
Phil Daintree <phil@logicworks.co.nz>
2014-08-09 09:35:59 +0100

    Make LETTER the default paper

--- includes/PDFStarter.php
+++ includes/PDFStarter.php
@@ -169,7 +169,15 @@ switch ($PaperSize) {
 		$Right_Margin = 25; // Half inch = 72/2
 		break;
 	default:
-		$DocumentOrientation = 'L';
+		$DocumentPaper = 'LETTER';
+		$DocumentOrientation ='L';
+
+		$Page_Width = 792; // 72 * 11 inch
+		$Page_Height = 612; // 72 * 8.5 inch
+		$Top_Margin = 36; // Half inch = 72/2
+		$Bottom_Margin = 36; // Half inch = 72/2
+		$Left_Margin = 36; // Half inch = 72/2
+		$Right_Margin = 36; // Half inch = 72/2
 		break;
 }
 

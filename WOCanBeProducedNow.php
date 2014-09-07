1aa15871fcfc3abca7adb579f630ca52be6d9748
Pak Ricard <pakricard@gmail.com>
2014-09-07 17:23:09 +0100

    new script listing all items in running WO that can be produced with available stock in location. Prints also single item production slip with basic BOM, components and image information.

---
+++ WOCanBeProducedNow.php
@@ -0,0 +1,194 @@
+<?php
+
+include('includes/session.inc');
+$Title = _('WO items can be produced with available stock');
+include('includes/header.inc');
+
+echo '<p class="page_title_text" align="center">
+		<img src="' . $RootPath . '/css/' . $Theme . '/images/reports.png" title="' . _('Search') . '" alt="" />
+		<strong>' . "List of items in WO ready to be produced in: " . '</strong>
+	</p>';
+
+if (isset($_POST['submit'])) {
+	submit($RootPath, $_POST['Location']);
+} else {
+	display();
+}
+
+//####_SUBMIT_SUBMIT_SUBMIT_SUBMIT_SUBMIT_SUBMIT_SUBMIT_SUBMIT_SUBMIT_SUBMIT_SUBMIT_SUBMIT####
+function submit($RootPath, $Location) {
+
+	$WhereLocation = " AND workorders.loccode >= '" . $Location . "' ";
+
+	$SQL = "SELECT woitems.wo,
+				woitems.stockid,
+				woitems.qtyreqd,
+				woitems.qtyrecd,
+				stockmaster.decimalplaces,
+				stockmaster.units
+			FROM workorders, woitems, stockmaster
+			WHERE workorders.wo = woitems.wo
+				AND stockmaster.stockid = woitems.stockid
+				AND workorders.closed = 0
+				AND woitems.qtyreqd > woitems.qtyrecd " . $WhereLocation . "ORDER BY woitems.wo, woitems.stockid";
+
+	$ErrMsg = _('The SQL to find the WO items to produce ');
+	$ResultItems = DB_query($SQL, $ErrMsg);
+	if (DB_num_rows($ResultItems) != 0) {
+		echo '<table class="selection">';
+		$TableHeader = '<tr>
+							<th>' . _('WO') . '</th>
+							<th>' . _('Stock ID') . '</th>
+							<th>' . _('Requested') . '</th>
+							<th>' . _('Received') . '</th>
+							<th>' . _('Pending') . '</th>
+							<th>' . _('UOM') . '</th>
+							<th>' . _('Component') . '</th>
+							<th>' . _('QOH') . '</th>
+							<th>' . _('Needed') . '</th>
+							<th>' . _('Shrinkage') . '</th>
+							<th>' . _('UOM') . '</th>
+							<th>' . _('') . '</th>
+							<th>' . _('Result') . '</th>
+						</tr>';
+
+		while ($myItem = DB_fetch_array($ResultItems)) {
+			echo $TableHeader;
+
+			$QtyPending = $myItem['qtyreqd'] - $myItem['qtyrecd'];
+			$QtyCanBeProduced = $QtyPending;
+
+			$WOLink = '<a href="' . $RootPath . '/WorkOrderEntry.php?WO=' . $myItem['wo'] . '">' . $myItem['wo'] . '</a>';
+			$CodeLink = '<a href="' . $RootPath . '/SelectProduct.php?StockID=' . $myItem['stockid'] . '">' . $myItem['stockid'] . '</a>';
+
+			printf('<td class="number">%s</td>
+					<td>%s</td>
+					<td class="number">%s</td>
+					<td class="number">%s</td>
+					<td class="number">%s</td>
+					<td>%s</td>
+					<td>%s</td>
+					<td class="number">%s</td>
+					<td class="number">%s</td>
+					<td class="number">%s</td>
+					<td>%s</td>
+					<td>%s</td>
+					<td>%s</td>
+					</tr>', $WOLink, $CodeLink, locale_number_format($myItem['qtyreqd'], $myItem['decimalplaces']), locale_number_format($myItem['qtyrecd'], $myItem['decimalplaces']), locale_number_format($QtyPending, $myItem['decimalplaces']), $myItem['units'], '', '', '', '', '', '', '');
+
+			// Get the BOM for this item
+			$SQLBOM = "SELECT bom.parent,
+						bom.component,
+						bom.quantity AS bomqty,
+						stockmaster.decimalplaces,
+						stockmaster.units,
+						stockmaster.shrinkfactor,
+						locstock.quantity AS qoh
+					FROM bom, stockmaster, locstock
+					WHERE bom.component = stockmaster.stockid
+						AND bom.component = locstock.stockid
+						AND locstock.loccode = '" . $Location . "'
+						AND bom.parent = '" . $myItem['stockid'] . "'
+						AND bom.effectiveafter < '" . Date('Y-m-d') . "'
+						AND (bom.effectiveto > '" . Date('Y-m-d') . "'
+						 OR bom.effectiveto='0000-00-00')";
+
+			$ErrMsg = _('The bill of material could not be retrieved because');
+			$BOMResult = DB_query($SQLBOM, $ErrMsg);
+			$ItemCanBeproduced = True;
+
+			while ($myComponent = DB_fetch_array($BOMResult)) {
+
+				$ComponentNeeded = $myComponent['bomqty'] * $QtyPending;
+				$PrevisionShrinkage = $ComponentNeeded * ($myComponent['shrinkfactor'] / 100);
+
+				if ($myComponent['qoh'] >= $ComponentNeeded) {
+					$Available = "OK";
+				} else {
+					$Available = "";
+					$ItemCanBeproduced = False;
+				}
+
+				$ComponentLink = '<a href="' . $RootPath . '/SelectProduct.php?StockID=' . $myComponent['component'] . '">' . $myComponent['component'] . '</a>';
+
+				printf('<td class="number">%s</td>
+					<td>%s</td>
+					<td class="number">%s</td>
+					<td class="number">%s</td>
+					<td class="number">%s</td>
+					<td>%s</td>
+					<td>%s</td>
+					<td class="number">%s</td>
+					<td class="number">%s</td>
+					<td class="number">%s</td>
+					<td>%s</td>
+					<td>%s</td>
+					<td>%s</td>
+					</tr>', '', '', '', '', '', '', $ComponentLink, locale_number_format($myComponent['qoh'], $myComponent['decimalplaces']), locale_number_format($ComponentNeeded, $myComponent['decimalplaces']), locale_number_format($PrevisionShrinkage, $myComponent['decimalplaces']), $myComponent['units'], $Available, '');
+			}
+			if ($ItemCanBeproduced) {
+				$Action = 'Produce ' . locale_number_format($QtyPending, 0) . ' x ' . $myItem['stockid'] . ' for WO ' . locale_number_format($myItem['wo'], 0);
+				$ComponentLink = '<a href="' . $RootPath . '/PrintWOItemSlip.php?StockId=' . $myItem['stockid'] . '&WO=' . $myItem['wo'] . '&Location=' . $Location . '">' . $Action . '</a>';
+			} else {
+				$ComponentLink = "";
+			}
+			printf('<td class="number">%s</td>
+					<td>%s</td>
+					<td class="number">%s</td>
+					<td class="number">%s</td>
+					<td class="number">%s</td>
+					<td>%s</td>
+					<td>%s</td>
+					<td class="number">%s</td>
+					<td class="number">%s</td>
+					<td class="number">%s</td>
+					<td>%s</td>
+					<td>%s</td>
+					<td>%s</td>
+					</tr>', '', '', '', '', '', '', '', '', '', '', '', '', $ComponentLink);
+		}
+		echo '</table>';
+
+	} else {
+		prnMsg('No items waiting to be produced in ' . $Location);
+	}
+
+} // End of function submit()
+
+
+function display() {
+	// Display form fields. This function is called the first time
+	// the page is called.
+
+	echo '<form action="' . htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, 'UTF-8') . '" method="post">';
+	echo '<input type="hidden" name="FormID" value="' . $_SESSION['FormID'] . '" />';
+
+	echo '<table>';
+
+	echo '<tr>
+				<td>' . _('For Factory Location') . ':</td>
+				<td><select name="Location">';
+
+	$SQL = "SELECT loccode,
+						locationname
+				FROM locations";
+
+	$LocnResult = DB_query($SQL);
+
+	while ($MyRow = DB_fetch_array($LocnResult)) {
+		echo '<option value="' . $MyRow['loccode'] . '">' . $MyRow['locationname'] . '</option>';
+	}
+	echo '</select></td>
+			</tr>';
+
+
+	echo '</table>
+		<div class="centre">
+			<input type="submit" name="submit" value="' . _('Search Items To Produce') . '" />
+		</div>';
+	echo '</form>';
+
+} // End of function display()
+
+include('includes/footer.inc');
+?>

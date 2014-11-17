3767ab2a2c8c47c048066770c4e592eaa7732e13
Tim Schofield <tim@weberpafrica.com>
2014-11-17 11:40:04 +0000

    Minor bug fixes found by colin test

--- WorkCentres.php
+++ WorkCentres.php
@@ -97,6 +97,7 @@ if (isset($_POST['submit'])) {
 			prnMsg(_('The selected work centre record has been deleted'), 'succes');
 		} // end of Contract BOM test
 	} // end of BOM test
+	unset($SelectedWC);
 }
 
 if (!isset($SelectedWC)) {
@@ -198,7 +199,7 @@ if (isset($SelectedWC)) {
 	echo '<table class="selection">
 			<tr>
 				<td>' . _('Work Centre Code') . ':</td>
-				<td><input type="text" name="Code" size="6" autofocus="autofocus" required="required" minlength="1" maxlength="5" value="' . $_POST['Code'] . '" /></td>
+				<td><input type="text" class="AlphaNumeric" name="Code" size="6" autofocus="autofocus" required="required" minlength="1" maxlength="5" value="' . $_POST['Code'] . '" /></td>
 			</tr>';
 }
 
@@ -216,7 +217,7 @@ if (!isset($_POST['Description'])) {
 }
 echo '<tr>
 		<td>' . _('Work Centre Description') . ':</td>
-		<td><input type="text" name="Description" size="21" required="required" minlength="1" maxlength="20" value="' . $_POST['Description'] . '" /></td>
+		<td><input type="text" name="Description" size="21" required="required" minlength="3" maxlength="20" value="' . $_POST['Description'] . '" /></td>
 	</tr>
 	<tr><td>' . _('Location') . ':</td>
 		<td><select required="required" minlength="1" name="Location">';

690931ba1b409db17b01db6d5284dc9195c00ebd
Pak Ricard <pakricard@gmail.com>
2014-11-25 16:00:21 +0000

    Minor bug fix

--- SalesCategories.php
+++ SalesCategories.php
@@ -323,7 +323,7 @@ echo '<table class="selection">
 		</tr>
 		<tr>
 			<td>' . _('Category Name') . ':</td>
-			<td><input type="text" name="SalesCatName" size="20" required="required" minlength="1" maxlength="20" value="' . $_POST['SalesCatName'] . '" /></td>
+			<td><input type="text" name="SalesCatName" size="20" required="required" minlength="1" maxlength="50" value="' . $_POST['SalesCatName'] . '" /></td>
 		</tr>';
 
 echo '<tr>

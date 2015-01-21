8a8577420c802cbd12ea54f58bc0eac21d835500
Rafael Emilio Chacon <rafael.emilio.chacon@gmail.com>
2015-01-21 11:51:23 +0000

    Add info to manual.

--- sql/updates/122.php
+++ sql/updates/122.php
@@ -1,7 +1,7 @@
 <?php
 
-AddColumn('needsrevision', 'stockdescriptiontranslations', 'INT(1)', 'NOT NULL', '0', 'descriptiontranslation');
-AddColumn('needsrevision', 'stocklongdescriptiontranslations', 'INT(1)', 'NOT NULL', '0', 'longdescriptiontranslation');
+AddColumn('needsrevision', 'stockdescriptiontranslations', 'TINYINT(1)', 'NOT NULL', '0', 'descriptiontranslation');
+AddColumn('needsrevision', 'stocklongdescriptiontranslations', 'TINYINT(1)', 'NOT NULL', '0', 'longdescriptiontranslation');
 
 NewScript('AutomaticTranslationDescriptions.php', '15');
 NewMenuItem('Utilities', 'Transactions', _('Automatic Translation - Item descriptions'), '/AutomaticTranslationDescriptions.php', 6);

a4daed56575fe0b932fd1840ebfe57c638369e30
Pak Ricard <pakricard@gmail.com>
2015-06-14 22:26:09 +0100

    Use of company decimal numbers instead of fixed decimal numbers

--- Z_CheckGLTransBalance.php
+++ Z_CheckGLTransBalance.php
@@ -16,18 +16,19 @@ $Header = '<tr>
 echo $Header;
 
 $SQL = "SELECT gltrans.type,
-		systypes.typename,
-		gltrans.typeno,
-		periodno,
-		SUM(amount) AS nettot
-	FROM gltrans,
-		systypes
-	WHERE gltrans.type = systypes.typeid
-	GROUP BY gltrans.type,
-		systypes.typename,
-		typeno,
-		periodno
-	HAVING ABS(SUM(amount))>0.01";
+			systypes.typename,
+			gltrans.typeno,
+			periodno,
+			SUM(amount) AS nettot
+		FROM gltrans,
+			systypes
+		WHERE gltrans.type = systypes.typeid
+		GROUP BY gltrans.type,
+			systypes.typename,
+			typeno,
+			periodno
+		HAVING ABS(SUM(amount))>= " . 1/pow(10,$_SESSION['CompanyRecord']['decimalplaces']) . "
+		ORDER BY gltrans.counterindex";
 
 $OutOfWackResult = DB_query($SQL);
 
@@ -43,11 +44,11 @@ while ($OutOfWackRow = DB_fetch_array($OutOfWackResult)) {
 		$RowCounter++;
 	}
 	echo '<tr>
-	<td><a href="' . $RootPath . '/GLTransInquiry.php?TypeID=' . urlencode($OutOfWackRow['type']) . '&TransNo=' . urlencode($OutOfWackRow['typeno']) . '">' . $OutOfWackRow['typename'] . '</a></td>
-	<td class="number">' . $OutOfWackRow['typeno'] . '</td>
-	<td class="number">' . $OutOfWackRow['periodno'] . '</td>
-	<td class="number">' . locale_number_format($OutOfWackRow['nettot'], 3) . '</td>
-	</tr>';
+			<td><a href="' . $RootPath . '/GLTransInquiry.php?TypeID=' . urlencode($OutOfWackRow['type']) . '&TransNo=' . urlencode($OutOfWackRow['typeno']) . '">' . $OutOfWackRow['typename'] . '</a></td>
+			<td class="number">' . $OutOfWackRow['typeno'] . '</td>
+			<td class="number">' . $OutOfWackRow['periodno'] . '</td>
+			<td class="number">' . locale_number_format($OutOfWackRow['nettot'], $_SESSION['CompanyRecord']['decimalplaces']) . '</td>
+		</tr>';
 
 }
 echo '</table>';

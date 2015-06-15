8997b58a587b85c91409d2b0ae73c5b00cf356ed
Tim Schofield <tim@weberpafrica.com>
2015-06-15 12:12:06 +0100

    Fix syntax error

--- sql/updates/133.php
+++ sql/updates/133.php
@@ -2,11 +2,11 @@
 
 CreateTable('loctransfercancellations',
 "CREATE TABLE `loctransfercancellations` (
-			`reference` INT(11) NOT NULL ,
-			`stockid` VARCHAR(20) NOT NULL ,
-			`cancelqty` DOUBLE NOT NULL ,
-			`canceldate` DATETIME NOT NULL ,
-			`canceluserid` VARCHAR(20) NOT NULL ),
+  `reference` INT(11) NOT NULL ,
+  `stockid` VARCHAR(20) NOT NULL ,
+  `cancelqty` DOUBLE NOT NULL ,
+  `canceldate` DATETIME NOT NULL ,
+  `canceluserid` VARCHAR(20) NOT NULL ,
   KEY `Index1` (`reference`, `stockid`),
   KEY `Index2` (`canceldate`, `reference`, `stockid`)
 )");

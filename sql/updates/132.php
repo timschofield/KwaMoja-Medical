49ddfa96204d25770e2a319b89be079da69c3012
Tim Schofield <tim@weberpafrica.com>
2015-07-30 12:15:25 +0100

    Changes to account groups to facilutate Francophile chart of accounts

---
+++ sql/updates/132.php
@@ -0,0 +1,21 @@
+<?php
+
+ChangeColumnType('groupname', 'accountgroups', 'VARCHAR(150)', 'NOT NULL', '');
+ChangeColumnType('group_', 'chartmaster', 'VARCHAR(150)', 'NOT NULL', '');
+ChangeColumnType('accountname', 'chartmaster', 'VARCHAR(150)', 'NOT NULL', '');
+ChangeColumnType('parentgroupname', 'accountgroups', 'VARCHAR(150)', 'NOT NULL', '');
+
+ChangeColumnType('groupcode', 'accountgroups', 'CHAR(10)', 'NOT NULL', '');
+ChangeColumnType('groupcode', 'chartmaster', 'CHAR(10)', 'NOT NULL', '');
+ChangeColumnType('parentgroupcode', 'accountgroups', 'CHAR(10)', 'NOT NULL', '');
+
+DB_IgnoreForeignKeys();
+DropPrimaryKey('accountgroups', array('groupname'));
+AddPrimaryKey('accountgroups', array('groupcode'));
+
+DropIndex('chartmaster', 'Group_');
+AddIndex(array('groupcode'),'chartmaster', 'Group');
+DB_ReinstateForeignKeys();
+UpdateDBNo(basename(__FILE__, '.php'));
+
+?>

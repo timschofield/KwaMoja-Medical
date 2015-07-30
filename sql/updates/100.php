e08ccb1aa12d46ca6410fba30764dfe88a640032
Tim Schofield <tim@weberpafrica.com>
2015-07-30 11:31:09 +0100

    Copy the purchase order template correctly

--- sql/updates/100.php
+++ sql/updates/100.php
@@ -5,10 +5,10 @@ AddColumn('authoriser', 'purchorders', 'VARCHAR(20)', 'NOT NULL', '', 'initiator
 $SQL = "UPDATE purchorders SET authoriser=initiator";
 $Result = DB_query($SQL);
 
-$DirHandle = dir('companies/');
+$DirHandle = dir($PathPrefix . 'companies/');
 while (false !== ($CompanyEntry = $DirHandle->read())) {
-	if (is_dir('companies/' . $CompanyEntry) and $CompanyEntry != '..' and $CompanyEntry != '' and $CompanyEntry != '.') {
-		copy('companies/' . $DefaultDatabase . '/FormDesigns/PurchaseOrder.xml', 'companies/' . $CompanyEntry . '/FormDesigns/PurchaseOrder.xml');
+	if (is_dir($PathPrefix . 'companies/' . $CompanyEntry) and $CompanyEntry != '..' and $CompanyEntry != '' and $CompanyEntry != '.') {
+		copy($PathPrefix . 'companies/' . $DefaultDatabase . '/FormDesigns/PurchaseOrder.xml', $PathPrefix . 'companies/' . $CompanyEntry . '/FormDesigns/PurchaseOrder.xml');
 	}
 }
 $DirHandle->close();

98b8bdfd34cc0d132686224fcb02b3f2f8551202
Tim Schofield <tim@weberpafrica.com>
2015-10-29 12:00:45 +0000

    Use correct theme variable

--- PDFPrintLabel.php
+++ PDFPrintLabel.php
@@ -268,7 +268,7 @@ if (isset($_POST['PrintLabels']) and $NoOfLabels > 0) {
 	$Title = _('Price Labels');
 	include('includes/header.inc');
 
-	echo '<p class="page_title_text"><img src="' . $RootPath . '/css/' . $Theme . '/images/customer.png" title="' . _('Price Labels') . '" alt="" />
+	echo '<p class="page_title_text"><img src="' . $RootPath . '/css/' . $_SESSION['Theme'] . '/images/customer.png" title="' . _('Price Labels') . '" alt="" />
          ' . ' ' . _('Print Price Labels') . '</p>';
 
 	if (!function_exists('gd_info')) {

556dedda62b6479e2ab5461b2ef0d3d661ee23f1
Tim Schofield <tim@weberpafrica.com>
2015-12-09 12:42:50 +0000

    Change type button to input control

--- StockCategories.php
+++ StockCategories.php
@@ -705,9 +705,8 @@ if (isset($SelectedCategory)) {
 }
 /* end if there is a category selected */
 
-echo '<br />
-		<div class="centre">
-			<button type="submit" name="submit">' . _('Enter Information') . '</button>
+echo '<div class="centre">
+			<input type="submit" name="submit" value="' . _('Enter Information') . '" />
 		</div>
 	</form>';
 

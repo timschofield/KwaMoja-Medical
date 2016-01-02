16ccc8ab2d89d85422729b3325f7898bcba74ac3
Tim Schofield <tim@weberpafrica.com>
2016-01-02 11:48:54 +0000

    Improvement to syntax checker

--- build/check_syntax.sh
+++ build/check_syntax.sh
@@ -5,13 +5,13 @@ cd $ROOT_DIR
 for f in `find . -name "*.php" -o -name "*.inc"`
 do
     newname=`echo $f | cut -c3-`
-    filename="$ROOT_DIR/$newname"
-    echo $FileName
+    FileName="$ROOT_DIR/$newname"
+#    echo $FileName
     output=$((php -l $FileName ) 2>&1)
 
     if [ $? != 0 ]
     then
-		echo '**Error** '$output >> ~/kwamoja$(date +%Y%m%d).log
-		echo '' >> ~/kwamoja$(date +%Y%m%d).log
+		echo '**Error** '$output
+		echo ''
     fi
 done

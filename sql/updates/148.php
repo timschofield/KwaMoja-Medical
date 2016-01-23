62e41f95e8564a9bb264ac5ebd0beb322907b7a0
Tim Schofield <tim@weberpafrica.com>
2016-01-23 16:32:28 +0000

    Update scripts not inserted properly

---
+++ sql/updates/148.php
@@ -0,0 +1,45 @@
+<?php
+
+NewScript('BackupDatabase.php', 15);
+NewScript('EDISendInvoices_Reece.php', 15);
+NewScript('GLBalanceSheet_new.php', 15);
+NewScript('InitialScripts.php', 15);
+NewScript('MailSalesReport.php', 15);
+NewScript('MenuManager.php', 15);
+NewScript('PDFAck.php', 15);
+NewScript('PDFShipLabel.php', 15);
+NewScript('PO_Chk_ShiptRef_JobRef.php', 15);
+NewScript('prlabout.php', 15);
+NewScript('prlCurrencies.php', 15);
+NewScript('prlDepartments.php', 15);
+NewScript('prlEditPayroll.php', 15);
+NewScript('prlEmployers.php', 15);
+NewScript('prlLoanFile.php', 15);
+NewScript('prlMsgBox.php', 15);
+NewScript('prlOthIncTable.php', 15);
+NewScript('prlOvertime.php', 15);
+NewScript('prlPaye.php', 15);
+NewScript('prlPositions.php', 15);
+NewScript('prlReNSSFPremium.php', 15);
+NewScript('prlRepBPPremium.php', 15);
+NewScript('prlRepGROSSPAYPremium.php', 15);
+NewScript('prlSC.php', 15);
+NewScript('prlSelectTaxStatus.php', 15);
+NewScript('prlSSC.php', 15);
+NewScript('prlTax.php', 15);
+NewScript('prlTaxAuthority.php', 15);
+NewScript('prlTaxAuthorityRates.php', 15);
+NewScript('ProjectCosting.php', 15);
+NewScript('QuickInvoice.php', 15);
+NewScript('ReportBug.php', 15);
+NewScript('report_runner.php', 15);
+NewScript('RetainedEarningsReconciliation.php', 15);
+NewScript('RunScheduledJobs.php', 15);
+NewScript('SearchCustomers.php', 15);
+NewScript('UpdateFavourites.php', 15);
+NewScript('Z_ImportCustBranch.php', 15);
+NewScript('Z_UpgradeDatabase.php', 15);
+
+UpdateDBNo(basename(__FILE__, '.php'));
+
+?>

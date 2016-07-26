144f9861ce8ff266bc9ab2f76351dd8d1232f1f8
Exson Qu <hexinfans@gmail.com>
2016-07-26 13:40:24 +0100

    Add InternalStockRequestInquiry.php

---
+++ sql/updates/159.php
@@ -0,0 +1,10 @@
+<?php
+
+
+NewScript('InternalStockRequestInquiry.php', '15');
+
+NewMenuItem('stock', 'Reports', _('Internal Stock Request Inquiry'), '/InternalStockRequestInquiry.php', 24);
+
+UpdateDBNo(basename(__FILE__, '.php'));
+
+?>
